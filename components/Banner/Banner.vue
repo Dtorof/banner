022c5da0c74e5a1cf29fae8edeb83853e6553a8a
Dtorof <dtoro.sistemas@gmail.com>
2023-02-27 15:43:34 -0500

    Meta

--- components/Banner/Banner.vue
+++ components/Banner/Banner.vue
@@ -1,5 +1,19 @@
 <script setup>
 import { ref, onMounted } from "vue";
+
+useHead({
+  title: 'Banner',
+  meta: [
+    { name: 'Carousel', content: 'My Banner site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [ { children: 'console.log(\'Hello world\')' } ]
+})
+
+
+
 const { data } = await useAsyncData("market2", () =>
     $fetch(
         `https://api.dev.inoutdelivery.com.co/v1/banners?business=ppc.inoutdelivery.com`
@@ -42,22 +56,24 @@ onMounted(() => {
         nextImage();
     }, 5000);
 });
+
 </script>
 
 <template>
+
     <div class="carousel">
         <transition name="slide">
-            <img :src="img[imgIndex]" :key="imgIndex" class="img-responsive">
+            <img :src="img[imgIndex]" :key="imgIndex" class="img-responsive" alt="Carousel">
         </transition>
 
         <div class="buttons">
-            <i @click="prevImage" class="animate__animated animate__fadeIn "><svg xmlns="http://www.w3.org/2000/svg"
+            <i @click="prevImage" class="animate__animated animate__fadeIn " aria-label="Right"><svg xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 256 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                     <path
                         d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
                 </svg></i>
 
-            <i @click="nextImage" class="animate__animated animate__fadeIn "><svg xmlns="http://www.w3.org/2000/svg"
+            <i @click="nextImage" class="animate__animated animate__fadeIn " aria-label="Left"><svg xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 256 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                     <path
                         d="M246.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 41.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
@@ -67,7 +83,7 @@ onMounted(() => {
         <div class="gallery-pagination">
             <ul>
                 <li v-for="(image, index) in img" :key="index">
-                    <button @click="imgIndex = index" :class="paginationSelect(index)"></button>
+                    <button @click="imgIndex = index" :class="paginationSelect(index)" aria-label="Pagination"></button>
                 </li>
 
             </ul>
